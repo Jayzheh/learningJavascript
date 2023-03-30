06deccf0b4265df7555cfd5a4ed06bb67388dadb
Jayzheh <danbayan@protonmail.com>
2023-03-30 02:31:35 +0200

    third exercise

---
+++ fizzBuzz.js
@@ -0,0 +1,25 @@
+/* Instructions 
+ *
+ */
+
+var output = []; // empty array
+var count = 1;
+
+function fizzBuzz() {
+	if (count % 3 === 0 && count % 5 === 0) {
+		output.push("FizzBuzz");
+	} 
+	else if (count % 3 === 0) {
+		output.push("Fizz");
+	} 
+	else if (count % 5 === 0) {
+		output.push("Buzz");
+	} 
+	else 
+		output.push(count);
+	count++;
+
+	console.log(output); // print out value of output every time it is run
+}
+
+fizzBuzz(); // call the fizzBuzz function
